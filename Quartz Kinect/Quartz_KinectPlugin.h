ec729b904bbb33739f1acd6bd81ec6a98cb1322d
Ryan Challinor <awwbees@Ryan-Challinors-MacBook-Pro.local>
2011-06-03 01:42:14 -0400

    Initial Commit

---
+++ Quartz Kinect/Quartz_KinectPlugin.h
@@ -0,0 +1,23 @@
+//
+//  Quartz_KinectPlugin.h
+//  Quartz Kinect
+//
+//  Created by Ryan Challinor on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface Quartz_KinectPlugIn : QCPlugIn {
+@private
+    
+}
+
+/*
+Declare here the properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
